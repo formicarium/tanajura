7e7d37f5614e8d1a8f748e83ca53e4e6cef52650
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-03 04:16:45 -0300

    clean repo 2

--- src/user/resolvers/mutation.ts
+++
@@ -1,7 +0,0 @@
-import { IContext } from '../../graphql/schema'
-
-export default {
-  createUser: async (value, args, ctx: IContext, info) => {
-    return ctx.components.prisma.db.mutation.createUser(args, info)
-  },
-}
