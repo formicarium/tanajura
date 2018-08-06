4fef1fe0ea32c2c4344ac48c35690fd9df16694c
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-08-06 00:25:17 -0300

    feat(general): blah

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const main = async () => {
   const system = new System<IComponents>(componentMap)
   const components: IComponents = await system.start()
   const { port } = components.config.getConfig().service
-  console.log(`System is up: http://localhost:${port}/health`)
+  console.log(`System is up: http://localhost:${port}/api/version`)
 }
 
 main()
