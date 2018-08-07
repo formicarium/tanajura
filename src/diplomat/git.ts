f69d87b76b0866aa6a846e022bde87bc7be62878
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-07 03:16:13 -0300

    feat(repoPushed): calling repoPushed on git eventMap

--- src/diplomat/git.ts
+++ src/diplomat/git.ts
@@ -1,9 +1,10 @@
 import { IEventMap } from './../components/git-server'
 import {  IComponents } from './../system'
 import { IPush } from '../components/git-server'
+import { repoPushed } from './http'
 
 export const eventMap: IEventMap<IComponents> = {
   push: async (push: IPush, components) => {
-    // await repoPushed(push, components)
+    await repoPushed(push, components.http)
   },
 }
