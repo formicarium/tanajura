aa826b6e4a9417ec77a332ed92f363d250a1ab76
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-10 03:16:42 -0300

    get repo controller

--- src/git/controller.ts
+++ src/git/controller.ts
@@ -2,6 +2,7 @@ import { IComponents } from './../system'
 import * as diplomat from '../diplomat/http'
 import { IPush } from '../components/git-server'
 import * as boom from 'boom'
+import { IRepo } from './model'
 
 export const newRepo = async (repo: string, { git }: IComponents): Promise<void> => {
   const exists = await git.repoExists(repo)
@@ -17,4 +18,14 @@ export const repoPushed = async (pushDescription: IPush, { http }: IComponents):
 
 export const deleteRepo = (repo: string, { git }: IComponents): Promise<void> => {
   return git.deleteRepo(repo)
+}
+
+export const getRepo = async (repo: string, { git }: IComponents): Promise<IRepo> => {
+  if (!await git.repoExists(repo))  {
+    throw boom.notFound(`${repo} not found`)
+  }
+
+  return {
+    name: repo,
+  }
 }
