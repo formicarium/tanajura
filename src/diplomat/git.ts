8c038f8a79c7e3ae2ba04d0356129a8bf3e5628f
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-04 00:10:01 -0300

    feat(git): Skip pull if commit message has [skip-pull]

--- src/diplomat/git.ts
+++ src/diplomat/git.ts
@@ -4,7 +4,6 @@ import { IPush } from '../components/git-server'
 import * as gitPushesController from '../git/controllers/pushes'
 
 const handlePush = async (pushDescription: IPush, components: IComponents) => {
-  console.log(`Received push in repository ${pushDescription.repo}`)
   try {
     await gitPushesController.pushReceived(pushDescription, components.http, components.config)
   } catch (err) {
